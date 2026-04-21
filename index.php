2e55f0395f1df1580838bdb0b28bb6586b524bcb
mamefat2004@gmail.com <adamarahma99@gmail.com>
2026-04-21 04:06:54 +0000

    menu principal

--- index.php
+++ index.php
@@ -160,3 +160,12 @@ function jsonToArray($data){
     $json = json_encode($data);
     file_put_contents('data.json', $json);
 }
+function menuPrincipal(){
+    echo "\n========================================\n";
+    echo "   GESTION DES INSCRIPTIONS - ÉCOLE 221\n";
+    echo "========================================\n";
+    echo "1. Gestion des étudiants\n";
+    echo "2. Gestion des formations\n";
+    echo "3. Quitter\n";
+    echo "========================================\n";
+}
